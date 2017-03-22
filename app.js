aff674431d4e230a39b206ff475c0651de1414f8
Shyanta Vleugel <Shyanta Vleugel>
2017-03-22 16:59:42 +0100

    Add Body-Parser

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var ejs = require('ejs');
+var bodyParser = require('body-parser');
 
-var homeRouter = require('./routes/home.js');
+var indexRouter = require('./routes/index');
+var resultsRouter = require('./routes/results');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -14,7 +19,8 @@ app.set('view engine', 'ejs');
 // Tell express which static files to serve
 app.use(express.static('public'));
 
-app.use('/', homeRouter);
+app.use('/', indexRouter);
+app.use('/results', resultsRouter);
 
 
 app.get('/*', function(req,res){
