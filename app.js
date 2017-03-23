7fcb22707dd0aaae59791270899f9928e7c22ea2
Shyanta Vleugel <Shyanta Vleugel>
2017-03-23 23:29:54 +0100

    Create detail page and apiCall based on form

--- app.js
+++ app.js
@@ -5,28 +5,33 @@ var path = require('path');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 
+//Routers
 var indexRouter = require('./routes/index');
 var resultsRouter = require('./routes/results');
 
 var app = express();
 
+// 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Set view engine to .ejs and tell app where these files are placed
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Tell express which static files to serve
 app.use(express.static('public'));
 
+// Tell app which routers to use when certain pages are opened
 app.use('/', indexRouter);
 app.use('/results', resultsRouter);
 
-
+// When the / has input that isn't defined above, render the 404 error page
 app.get('/*', function(req,res){
   res.render('404');
 });
 
+// Let App listen to localhost:3000
 app.listen(3000, function(){
   console.log('De app is gestart');
 });
--- routes/results.js
+++ routes/results.js
@@ -1,24 +1,34 @@
 var express = require('express');
 var request = require('request');
-var bodyParser = require('body-parser');
 
 var router = express.Router();
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: true}));
 
-
-var api = 'http://funda.kyrandia.nl/feeds/Aanbod.svc/json/' + process.env.KEY + '/?type=koop&zo=/amsterdam/tuin/&page=1&pagesize=25';
-console.log(process.env.KEY);
 
 router.post('/', function(req,res){
 	var type = req.body.type;
-		keywords = req.body.keywords;
-	console.log(keywords);
+		woonplaats = req.body.woonplaats;
+		price_min = req.body.price_min;
+		price_max = req.body.price_max;
+
+	var api = 'http://funda.kyrandia.nl/feeds/Aanbod.svc/json/' + process.env.KEY + '/?type=' + type +'&zo=/' + woonplaats + '/' + price_min + '+' + price_max + '/&page=1&pagesize=25';
+	console.log(api);
 	request(api, function (error, response, data) {
 		data = JSON.parse(data); 
 		res.locals.data = data.Objects;
 		res.render('results');
+	});
+});
+
+
+router.get('/:index', function(req,res){
+	var id = req.params.index;
+
+	var detailApi = "http://funda.kyrandia.nl/feeds/Aanbod.svc/json/detail/" + process.env.KEY + '/' + 'koop' + '/' + id;
+	request(detailApi, function(error, response, data) {
+		data = JSON.parse(data);
+		res.locals.data = data;
+		res.render('detail');
 	});
 });
 
