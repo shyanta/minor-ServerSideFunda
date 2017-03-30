5231f6b2703697239bab090f6c78fc71e15e3a13
Shyanta Vleugel <Shyanta Vleugel>
2017-03-30 12:40:31 +0200

    Add SortFunction, without importance

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # minor-ServerSideFunda
+
+- http://stackoverflow.com/questions/282670/easiest-way-to-sort-dom-nodes
+- https://github.com/Teun/thenBy.js
+- https://github.com/TimoVerkroost/minor-performance-matters-funda
--- public/js/sort.js
+++ public/js/sort.js
@@ -1,90 +1,116 @@
 var sortForm = document.getElementById('sort-form');
 
-var tuin = document.getElementById('tuin');
+var tuinId = document.getElementById('tuin');
 var slaapkamer = document.getElementById('slaapkamers');
-var garage = document.getElementById('garage');
+var garageId = document.getElementById('garage');
 var woon = document.getElementById('woonoppervlakte');
 var perceel = document.getElementById('perceeloppervlakte');
 
-var items = document.querySelectorAll('.house li');
-console.log(items)
+var list = document.querySelector('.house');
+
+var items = list.childNodes;
+var itemsArr = [];
+for (var i in items) {
+    if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
+        itemsArr.push(items[i]);
+    }
+}
+
+console.log(itemsArr);
 
 sortForm.addEventListener('submit', function(){
 	event.preventDefault();
-	var tuinVal = tuin.options[tuin.selectedIndex].value;
+	var tuinVal = tuinId.options[tuinId.selectedIndex].value;
 	var slaapKamerVal = slaapkamer.options[slaapkamer.selectedIndex].value;
-	var garageVal = garage.options[garage.selectedIndex].value;
+	var garageVal = garageId.options[garageId.selectedIndex].value;
 	var woonOppVal = woon.options[woon.selectedIndex].value;
 	var perceelOppVal = perceel.options[perceel.selectedIndex].value;
 
-	console.log(items.attributes);
-	var itemsSorted = items.sort(function(a,b){
-		garage(a,b);
-	})
-	console.log(itemsSorted);
-	function Tuin(a,b){
-		console.log(a);
-		console.log(b);
-			if ((a.getAttribute("data-tuin") === false) && (b.getAttribute("data-tuin") === false)){
+	function Val(){
+		aantalKamers();
+	}
+
+	var itemsSorted = itemsArr.sort(
+		firstBy(aantalKamers)
+		.thenBy(tuin)
+		.thenBy(garage)
+		.thenBy(perceelOpp)
+		.thenBy(woonOpp)
+	);
+	for (i = 0; i < itemsArr.length; ++i) {
+		list.appendChild(itemsArr[i]);
+	}
+	console.log(itemsArr);
+
+	function tuin(a,b){
+		// console.log(a.dataset.tuin);
+			if ((a.dataset.tuin === false) && (b.dataset.tuin === false)){
 				return 0;
 			}
-			else if (a.getAttribute("data-tuin") === false){
+			else if (a.dataset.tuin === false){
 				return 1;
 			}
-			else if (b.getAttribute("data-tuin") === false){
+			else if (b.dataset.tuin === false){
 				return -1;
 			}
 	}
-	function AantalKamers(a,b){
+	function aantalKamers(a,b){
+		var x = Number(a.dataset.kamer);
+		var y = Number(b.dataset.kamer);
 		var kamersInput = document.querySelector('input[name="slaapkamersAantal"]').value;
 			kamersInputNum = Number(kamersInput);
-			kamersInputNum = 3;
-		if ((a.dataset.kamer === kamersInputNum) && (b.dataset.kamer === kamersInputNum)){
+		if ((x === kamersInputNum) && (y === kamersInputNum)){
+			console.log('0');
 			return 0;
 		}
-		else if (a.dataset.kamer === kamersInputNum){
+		else if (x === kamersInputNum){
+			console.log('1');
 			return -1;
 		}
-		else if (b.dataset.kamer === kamersInputNum){
+		else if (y === kamersInputNum){
+			console.log('-1');
 			return 1;
 		}
 	}
-	function Garage(a,b){
-		if ((a.Garage === false) && (b.Garage === false)){
+	function garage(a,b){
+		console.log(a.dataset.garage);
+		if ((a.dataset.garage === false) && (b.dataset.garage === false)){
 			return 0;
 		}
-		else if (a.Garage === false){
+		else if (a.dataset.garage === false){
 			return 1;
 		}
-		else if (b.Garage === false){
+		else if (b.dataset.garage === false){
 			return -1;
 		}
 	}
 	function woonOpp(a,b){
+		var x = Number(a.dataset.woonopp);
+		var y = Number(b.dataset.woonopp);
 		var woonOppMin = document.querySelector('input[name="woonoppmin"]').value;
 			woonOppMinNum = Number(woonOppMin);
-			woonOppMinNum = 200;
-		if ((a.dataset.woonopp < woonOppMinNum) && (b.dataset.woonopp < woonOppMinNum)){
+		if ((x < woonOppMinNum) && (y < woonOppMinNum)){
 			return 0;
 		}
-		else if (a.dataset.woonopp > woonOppMinNum){
+		else if (x > woonOppMinNum){
 			return -1;
 		}
-		else if (b.dataset.woonopp > woonOppMinNum){
+		else if (y > woonOppMinNum){
 			return 1;
 		}
 	}
 	function perceelOpp(a,b){
+		var x = Number(a.dataset.perceelopp);
+		var y = Number(b.dataset.perceelopp);
 		var perceelOppMin = document.querySelector('input[name="perceeloppmin"]').value;
 			perceelOppMinNum = Number(perceelOppMin);
-			perceelOppMinNum = 250;
-		if ((a.dataset.perceelopp < perceelOppMinNum) && (b.dataset.perceelopp < perceelOppMinNum)){
+		if ((x < perceelOppMinNum) && (y < perceelOppMinNum)){
 			return 0;
 		}
-		else if (a.dataset.perceelopp > perceelOppMinNum){
+		else if (x > perceelOppMinNum){
 			return -1;
 		}
-		else if (b.dataset.perceelopp > perceelOppMinNum){
+		else if (y > perceelOppMinNum){
 			return 1;
 		}
 	}
